aa4cc298647a93367041f4a5a816810aa8a266b1
xerzen <xerzen@live.com>
2020-08-07 10:48:48 -0700

    split up files and functions to modulate code

--- IG_RECIPROCATE/ig_selenium.py
+++ IG_RECIPROCATE/ig_selenium.py
@@ -11,11 +11,13 @@ from selenium.webdriver.support import expected_conditions as EC
 
 
 class reciprocate:
-
     def __init__(self, username, password):
         self.username = username
         self.password = password
-        self.browser = webdriver.Chrome()
+        
+        chrome_options = Options()  #run browser in the background
+        chrome_options.add_argument("--headless")  
+        self.browser = webdriver.Chrome('./assets/chromedriver', chrome_options=chrome_options)
 
     def endReciprocate(self):
         self.browser.close()
@@ -31,9 +33,7 @@ class reciprocate:
         userINPUT = browser.find_element_by_xpath("//input[@name='username']")
         userINPUT.clear()
         userINPUT.send_keys(self.username)
-        #------------------------------------
-
-        # !!!! try with pressing tab instead of find password path
+        #---------------------------------
         passINPUT = browser.find_element_by_xpath("//input[@name='password']")
         passINPUT.clear()
         passINPUT.send_keys(self.password)
@@ -43,22 +43,26 @@ class reciprocate:
 
     def getFollowers(self, count):
         followerCount = count
+
+        #open up followers window
         browser = self.browser
         followersList = browser.find_element_by_xpath("//*[@id='react-root']/section/main/div/header/section/ul/li[2]/a")
-        followersList.click()
+        followersList.click()   
         time.sleep(2)
 
+        #scroll through followers window to load all followers
         fBody = browser.find_element_by_xpath("//div[@class='isgrP']")
-        while count > 0:
+        while followerCount > 0:
             browser.execute_script('arguments[0].scrollTop = arguments[0].scrollTop + arguments[0].offsetHeight;', fBody)
             time.sleep(.5)
-            count -= 6
+            followerCount -= 5
 
-        #prints out all the followers in pop-up window
-        followersDATA = browser.find_element_by_xpath("/html/body/div[3]/div/div[2]")
+        #get all the data in pop-up window
+        followersDATA = browser.find_element_by_xpath("/html/body/div[4]/div/div/div[2]/ul/div")
         followersDATA = followersDATA.text
 
-        followersList = extractProfiles(followersDATA, followerCount)
+        #extract followers and return as array
+        followersList = self.extractProfiles(followersDATA, count)
         browser.get("https://www.instagram.com/" + self.username + '/')
         time.sleep(2)
         return followersList
@@ -66,135 +70,48 @@ class reciprocate:
 
     def getFollowing(self, count):
         followCount = count
+
+        #open up following pop-window
         browser = self.browser
         followingList = browser.find_element_by_xpath("//*[@id='react-root']/section/main/div/header/section/ul/li[3]/a")
         followingList.click()
         time.sleep(2)
 
+        #scroll through following pop-window to load all followers
         fBody = browser.find_element_by_xpath("//div[@class='isgrP']")
-        while count > 0:
+        while followCount > 0:
             browser.execute_script('arguments[0].scrollTop = arguments[0].scrollTop + arguments[0].offsetHeight;', fBody)
             time.sleep(.5)
-            count -= 5
+            followCount -= 5
 
-        # prints out all the followers in pop-up window
-        followingDATA = browser.find_element_by_xpath("/html/body/div[3]/div/div[2]")
+        #get data in pop-up window as text
+        followingDATA = browser.find_element_by_xpath("/html/body/div[4]/div/div/div[2]/ul/div")
         followingDATA = followingDATA.text
 
-        followingList = extractProfiles(followingDATA, followCount)
+        #extract following and return as array
+        followingList = self.extractProfiles(followingDATA, count)
         browser.get("https://www.instagram.com/" + self.username + '/')
         time.sleep(2)
         return followingList
 
+    def extractProfiles(self, s, limit):
+        usernames = []
+        arr = s.split('\n')
+
+        for i, name in enumerate(arr):
+            if (arr[i-1] == 'Following' or arr[i-1] == 'Follow' or arr[i-1] == 'Requested'):
+                usernames.append(name)
+
+        return sorted(usernames)
 
     def getProfileStats(self):
         browser = self.browser
         browser.get("https://www.instagram.com/" + self.username + '/')
         time.sleep(3)
 
-        profile_elem = browser.find_element_by_xpath("//span[@id = 'react-root']")
+        #extract profile stats
+        profile_elem = browser.find_element_by_xpath("//*[@id='react-root']/section/main/div")
         profile = profile_elem.text
         profile_arr = profile.split('\n')
         return profile_arr[:7]
-
-    def unfollowUsers(self, unfollowArr):
-        browser = self.browser
-        for user in unfollowArr:
-            browser.get("https://www.instagram.com/" + user + '/')
-            time.sleep(3)
-            unfollowUser = browser.find_element_by_xpath("//*[@id='react-root']/section/main/div/ header / section / div[1] / button")
-            unfollowUser.click()
-
-            time.sleep(5)
-
-
-
 # ---------------------------
-def unfollowing(followers, following):
-    unfollowArr = []
-    index = 0
-    for user in following:
-        followingBack = False
-        while True:
-            if followers[index] == user:
-                followingBack = True
-                break
-            elif followers[index] > user:
-                break
-            index += 1
-        if not followingBack:
-            unfollowArr.append(user)
-
-    return unfollowArr
-
-def extractProfiles(s, limit):
-    usernames = []
-    arr = s.split('\n')
-
-    index = 0
-    while limit > 0:
-        usernames.append(arr[index])
-        if index + 1 > len(arr):
-            break
-        elif arr[index + 1] == 'Verified':
-            index += 1
-
-        if isProfileName(arr[index + 1]):
-            index += 2
-        else:
-            index += 3
-        limit -= 1
-    return sorted(usernames)
-
-def isProfileName(s):
-    if (s == 'Following') or (s == 'Follow') or (s == 'Requested'):
-        return True
-    else:
-        return False
-
-def extractCount(s):
-    arr = s.split()
-    return arr[0]
-
-if __name__ == "__main__":
-
-    username = input("USERNAME: ")
-    password = input("PASSWORD: ")
-
-    ig = reciprocate(username, password)
-    ig.login()
-
-    profile = ig.getProfileStats()
-    profileName = profile[5]
-    profileBio = profile[6]
-    followersCount = extractCount(profile[3])
-    followingCount = extractCount(profile[4])
-    postsCount = extractCount(profile[2])
-
-    print("Profile Name: " + profileName)
-    print("Profile Bio: " + profileBio)
-    print("Followers: " + followersCount)
-    print("Following: " + followingCount)
-
-    followers = ig.getFollowers(int(followersCount))
-
-    print("----------------------------------------------------------")
-
-    following = ig.getFollowing(int(followingCount))
-
-    print(followers,'\n',following)
-
-    unfollow = unfollowing(followers, following)
-
-    for i in unfollow:
-        print (i)
-
-    print("unreciprocated count: ", len(unfollow))
-
-
-    closeAuthorization = input("press q to close: ")
-    if closeAuthorization == 'q':
-        ig.endReciprocate()
-
-
-
